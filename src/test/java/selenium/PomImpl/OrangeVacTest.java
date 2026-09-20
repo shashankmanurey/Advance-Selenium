09d94988d0de40c4a36d1c6a1fac1fc8f8642d82
shashankmanurey <shashankmanurey@gmail.com>
2026-09-20 14:56:25 +0530

    updated OrangeVacTest

--- src/test/java/selenium/PomImpl/OrangeVacTest.java
+++ src/test/java/selenium/PomImpl/OrangeVacTest.java
@@ -3,15 +3,14 @@ package selenium.PomImpl;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-import org.openqa.selenium.WebDriver;
-
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import selenium.advance.BaseClassUtility.OrangeBase1;
@@ -21,29 +20,44 @@ import selenium.PomUtilities.OrangeVac;
 
 public class OrangeVacTest extends OrangeBase1 {
 
-    @Test
-    public void createVacancy() throws Exception {
+    @DataProvider
+    public Object[][] excel() throws EncryptedDocumentException, IOException {
 
-        // Step 1: Read data from Excel
-        FileInputStream f = new FileInputStream("src/main/resources/DDT/OrangeExcel.xlsx");
+        FileInputStream ex = new FileInputStream("src/main/resources/DDT/OrangeExcel.xlsx");
 
-        Workbook wb = WorkbookFactory.create(f);
-
+        Workbook wb = WorkbookFactory.create(ex);
         Sheet sh = wb.getSheet("Sheet1");
-        Row row = sh.getRow(1);
+
+        int rc = sh.getLastRowNum();
+        int cc = sh.getRow(0).getLastCellNum();
+
+        Object[][] obj = new Object[rc][cc];
 
         DataFormatter df = new DataFormatter();
 
-        String vacancyName = df.formatCellValue(row.getCell(0));
-        String jobTitle = df.formatCellValue(row.getCell(1));
-        String description = df.formatCellValue(row.getCell(2));
-        String hiringManager = df.formatCellValue(row.getCell(3));
-        String numberOfPositions = df.formatCellValue(row.getCell(4));
+        for (int r = 1; r <= rc; r++) {
+            for (int c = 0; c < cc; c++) {
+
+                obj[r - 1][c] =
+                    df.formatCellValue(sh.getRow(r).getCell(c));
+            }
+        }
 
         wb.close();
-        f.close();
+        ex.close();
 
-        // Step 2: Login
+        return obj;
+    }
+    @Test(dataProvider = "excel")
+    public void createVacancy(
+        String vacancyName,
+        String jobTitle,
+        String description,
+        String hiringManager,
+        String numberOfPositions
+    ) throws Exception {
+
+        // Step 1: Login
         OrangeLogin login = new OrangeLogin(d);
 
         login.getun("Admin");
@@ -55,14 +69,14 @@ public class OrangeVacTest extends OrangeBase1 {
             "Login failed: Dashboard not displayed"
         );
 
-        // Step 3: Navigate to Recruitment - Vacancies
+        // Step 2: Navigate to Recruitment - Vacancies
         OrangeRect rect = new OrangeRect(d);
 
         rect.clickRecruitment();
         rect.clickvac();
         rect.clickadd();
 
-        // Step 4: Fill vacancy form using Excel data
+        // Step 3: Fill vacancy form using Excel data
         OrangeVac vac = new OrangeVac(d);
 
         vac.enterVacancy(vacancyName);
@@ -71,7 +85,7 @@ public class OrangeVacTest extends OrangeBase1 {
         vac.selectHM(hiringManager);
         vac.enterNumPos(numberOfPositions);
 
-        // Step 5: Save vacancy
+        // Step 4: Save vacancy
         vac.clickSave();
     }
 }
